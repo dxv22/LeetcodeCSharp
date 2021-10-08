0468635cfa2285b63564f8cfe153de6b4d21f152
Daxxon Vong <Daxxonvong@yahoo.co.nz>
2021-10-08 23:42:46 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace LeetcodeCSharp
+{
+    class Program
+    {
+        // Reverse String
+        public char[] ReverseString(char[] s)
+        {
+            int i = 0;
+            int j = s.Length - 1;
+            char curr;
+
+            while (i <= j)
+            {
+                curr = s[i];
+                s[i] = s[j];
+                s[j] = curr;
+                i++;
+                j--;
+            }
+            return s;
+        }
+
+        public string Test()
+        {
+            return "ADF";
+        }
+
+        static void Main(string[] args)
+        {
+            Program test = new Program();
+
+            // String reverse
+            char[] test1 = { 'A', 'B', 'C', 'D' };
+            Console.WriteLine(test.ReverseString(test1));
+        }
+    }
+}
